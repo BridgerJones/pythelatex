09a530fb5c1c7d9c8fef977da7e03ee0f4662c1d
Bridger Jones <bridgerjones15@gmail.com>
2020-10-23 15:09:04 -0600

    fixing strip in to_base_10, changing * to \times

--- commands.py
+++ commands.py
@@ -9,19 +9,19 @@ def to_base_10(base, any_base_number):
         right_offset = right_length + 1
 
         for char in any_base_number[0]:
-            latex_string += f"({char} * {base}^{left_length})+"
+            latex_string += f"({char} \\times {base}^{left_length})+"
             left_length -= 1
         for char in any_base_number[1]:
-            latex_string += f"({char} * {base}^{ {right_length - right_offset} })+"
+            latex_string += f"({char} \\times {base}^{ {right_length - right_offset} })+"
             right_length -= 1
-        print(latex_string.strip("+").replace("\n", ""))
+        print(latex_string.rstrip("+").replace("\n", ""))
     else :
         latex_string = ""
         left_length = len(any_base_number[0]) - 1
 
 
         for char in any_base_number[0]:
-            latex_string += f"({char} * {base}^{left_length})+"
+            latex_string += f"({char} \\times {base}^{left_length})+"
             left_length -= 1
 
-        print(latex_string.strip("+").replace("\n", ""))
+        print(latex_string.rstrip("+").replace("\n", ""))
